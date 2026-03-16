4bfce4e89fd616424a4f2ed0ab1b7ace0411e3c5
jaiderT <jaideralexandertotenabustamn@gmail.com>
2026-03-16 13:49:35 -0500

    Actualizado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Login from "./components/Auth/Login.jsx"
+import Prueba from "./components/Auth/Prueba.jsx"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <Routes>
 
       <Route path='/login' element={<Login />} />
+      <Route path='/prueba' element={<Prueba />} />
 
     </Routes>
     </BrowserRouter>
