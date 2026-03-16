b5c9c5cc28da507974e32d9f7a965037e34c2624
veronica vidal <vidalveropolania@gmail.com>
2026-03-16 15:07:38 -0500

    pagina inicio,navbar y footer arreglado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./components/Auth/Login.jsx"
-import Navbar from "./components/Layout/Navbar.jsx"
 import Inicio from "./components/Home/Inicio.jsx"
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='/login' element={<Login />} />
