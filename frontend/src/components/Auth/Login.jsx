d95daa45c8ad56d308603548c29c646fa2593f79
Ambiente 106 <ambiente106@iMac-de-CADPH.local>
2026-03-20 14:29:59 -0500

    conectando backend

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -8,7 +8,13 @@ export default function Login() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(null);
+  const [saliendo, setSaliendo] = useState(false);
   const navigate = useNavigate();
+
+  const handleNavigateRegister = () => {
+    setSaliendo(true);
+    setTimeout(() => navigate('/register'), 500);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -53,7 +59,7 @@ export default function Login() {
 
       {/* Flecha salir - visible en móvil y desktop */}
       <a href="/" className="absolute top-4 right-4 z-50 bg-[#3D1F0F]/60 p-2 rounded-full lg:bg-transparent lg:p-0">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="w-6 h-6 lg:w-7 lg:h-7 fill-white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" className="w-6 h-6 lg:w-7 lg:h-7 ">
           <path d="M224 160C241.7 160 256 145.7 256 128C256 110.3 241.7 96 224 96L160 96C107 96 64 139 64 192L64 448C64 501 107 544 160 544L224 544C241.7 544 256 529.7 256 512C256 494.3 241.7 480 224 480L160 480C142.3 480 128 465.7 128 448L128 192C128 174.3 142.3 160 160 160L224 160zM566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L438.6 169.3C426.1 156.8 405.8 156.8 393.3 169.3C380.8 181.8 380.8 202.1 393.3 214.6L466.7 288L256 288C238.3 288 224 302.3 224 320C224 337.7 238.3 352 256 352L466.7 352L393.3 425.4C380.8 437.9 380.8 458.2 393.3 470.7C405.8 483.2 426.1 483.2 438.6 470.7L566.6 342.7z"/>
         </svg>
       </a>
@@ -96,7 +102,7 @@ export default function Login() {
       </div>
 
       {/* PANEL DERECHO - full en móvil, fijo en desktop */}
-      <div className="w-full lg:w-[680px] bg-[#FAF7F2] flex flex-col justify-center px-6 py-12 sm:px-10 shrink-0">
+      <div className="w-full lg:w-[680px] bg-[#FAF7F2] flex flex-col justify-center px-6 py-12 sm:px- shrink-0 -mt-57">
 
         {/* Logo visible solo en móvil */}
         <div className="flex items-center gap-3 mb-8 lg:hidden">
@@ -108,7 +114,7 @@ export default function Login() {
           <button className="flex-1 py-2.5 rounded-lg bg-[#3B1F0A] text-white text-sm font-semibold">
             Iniciar sesión
           </button>
-          <button type="button" onClick={() => navigate('/register')} className="flex-1 py-2.5 rounded-lg text-gray-400 text-sm font-semibold">
+          <button type="button" onClick={handleNavigateRegister} className="flex-1 py-2.5 rounded-lg text-gray-400 text-sm font-semibold">
             Crear cuenta
           </button>
         </div>
