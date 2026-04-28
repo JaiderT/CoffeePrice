731bc3c7870a3858c354142b38a48280a304a306
EsneiderTT <esneidertrujillo054@gmail.com>
2026-04-28 15:17:10 -0500

    navbar

--- frontend/src/components/Layout/LayoutPublico.jsx
+++ frontend/src/components/Layout/LayoutPublico.jsx
@@ -1,14 +1,14 @@
 import Footer from './Footer';
 import Kaffi from '../kaffi';
+import Navbar from './Navbar';
 
 function LayoutPublico({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
-
+      <Navbar />
       <main className="grow">
         {children}
       </main>
-
       <Footer />
       <Kaffi />
     </div>
